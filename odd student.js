8bfe5d07409b3ab20b1fcf0cb100de1d8e7cea1f
Mosiur12 <mrsojib9600@gmail.com>
2022-02-05 22:31:15 +0600

    changes of code

--- odd student.js
+++ odd student.js
@@ -1,2 +1,20 @@
 // PROBLEM-4:
 // var students = ["tanjil", "robin","dipu","Mustafizz"];
+// let names ="";
+function oddFriend (friendsName){ 
+
+    for (i = 0; i<friendsName.length; i++){
+        let lengthOfName = friendsName[i].length;
+
+        if (lengthOfName % 2 == 1){
+            names = friendsName[i];
+            return names;
+            
+            
+        }
+        
+    }
+   return "There is no odd number element ";
+
+}
+console.log(oddFriend(["tanjil", "jim","robin","dipu", "fizz"]));
