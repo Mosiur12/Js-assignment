1a18fbada73cf098ebf2df8c4c15103c93380c18
Mosiur12 <mrsojib9600@gmail.com>
2022-02-05 16:10:03 +0600

    asign

---
+++ picnic.js
@@ -0,0 +1,31 @@
+// first 100 of students chada = 5000 tk;
+// second 100 of students chada = 4000 tk;
+// third 100 of students chada = 3000 tk;
+
+function picnicBudget(numberOfStudent){
+let budgetOfPicnic;
+if(numberOfStudent > 200){
+    let forFirst100Student = 100 * 5000;
+    let second100Student = 100 * 4000;
+    let third100Student = (numberOfStudent-200)*3000;
+
+    // Calculation The Budget
+
+    budgetOfPicnic = forFirst100Student + second100Student + third100Student;
+
+}
+
+else if (numberOfStudent > 100){
+    let forFirst100Student = 100 * 5000;
+    let second100Student = (numberOfStudent - 100)* 4000;
+
+    // Calculation The Budget
+    budgetOfPicnic = forFirst100Student + second100Student;
+}
+else if (numberOfStudent <= 100){
+    budgetOfPicnic = numberOfStudent * 5000;
+}
+return budgetOfPicnic;
+
+}
+console.log( picnicBudget(150));
---
+++ problem.js
@@ -0,0 +1,48 @@
+// PROBLEM-1
+
+// function anaToVori (ana){
+    
+//     if (Number.isNaN (ana)){
+//       var vori = ana/16;
+//       return vori;
+      
+//     }
+//     else{
+//       return "please put a intizer number";
+//     }
+    
+       
+// }
+// var voriToAna = anaToVori (64);
+// console.log(voriToAna);
+// (typeof ana === 'number')
+
+
+// ----------------PROBLEM-2---------:
+// sigaracost = 7 tk/singara
+// somuchacost = 10 tk/somucha
+// zilapicost = 15 tk/zilapi 
+
+
+// function pandaCost (singaraQuantity , somuchaQuantity , zilapiQuantity){
+// const perSingara = 7;
+// const perSomucha = 10;
+// const perZilapi = 15;
+
+// // Food Calculation
+// const costOfSingara = singaraQuantity * perSingara;
+// const costOfSomucha = somuchaQuantity * perSomucha;
+// const costOfZilapi = zilapiQuantity * perZilapi;
+
+// const totalCost = costOfSingara + costOfSomucha + costOfZilapi;
+
+
+// return totalCost;
+
+// }
+
+// const foodCost = pandaCost (1 , 2, 2);
+// console.log(foodCost);
+
+// PROBLEM-3:
+
